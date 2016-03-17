dd5eb08c4db62023aaa10347c885a085c1dcb702
Code-Lyoko <mmszek3000@gmail.com>
2016-03-17 19:33:03 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function Game(element) {
-  this.element = element
-  Crafty.c("HarryPotter", {
-    init: function(){
-      this.addComponent("2D, Canvas, Color");
-    }
-  })
+    this.element = element
+    Crafty.init(500/*px width*/,500/*px height*/,this.element/*: game element*/);
+    Crafty.c("HarryPotter", {
+        required: "2D, Canvas, Color",
+        init: function(){
+            
+        },
+    })
 }
